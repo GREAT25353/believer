9d6d0015ab98e46d02224c48e6d3b96446db83a2
GREAT25353 <yakash.dubey917@gmail.com>
2024-06-13 12:53:12 +0530

    java-commit

---
+++ h-repo/budhiya.java
@@ -0,0 +1,9 @@
+public class budhiya
+{
+    public static void main(String[] args) {
+        int a=5;
+        int b=7;
+        int c=a+b;
+        System.out.println("The sum of " + a + " and " + b + " is " + c);
+    }
+}
